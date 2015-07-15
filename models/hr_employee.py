e17f6171629120a6ccdd69234035383839e9b89b
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-07-15 15:21:48 +0300

    Add placeholders

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -5,3 +5,13 @@ from openerp import models, api, fields
 class HrEmployee(models.Model):
 
     _inherit = 'hr.employee'
+
+    group_sales = fields.Selection(selection='get_group_sales')
+
+    def get_group_sales(self):
+        group = [('a', 'A')]
+
+        if self.user_id:
+            group = self.user_id
+
+        return group
