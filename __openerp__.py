4c18efaa549bc4360f7e3108814f665112ca1011
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-15 17:00:36 +0300

    Update version number

--- __openerp__.py
+++ __openerp__.py
@@ -15,7 +15,7 @@
 {
     'name': 'HR User',
     'category': 'CRM',
-    'version': '0.3',
+    'version': '8.0.0.1.0',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': [
