4023861c8de673131bca5961213fcbcaf808dc29
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-24 11:24:15 +0300

    Add translations

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'HR User',
     'category': 'CRM',
-    'version': '8.0.0.9.8',
+    'version': '8.0.0.9.9',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': [
