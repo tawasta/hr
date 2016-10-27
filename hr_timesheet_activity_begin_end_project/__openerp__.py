ec80afc9e046457cf42634fba042a88de8ab1f58
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-10-27 10:39:03 +0300

    Add new module for Timesheet Activities on projects

---
+++ hr_timesheet_activity_begin_end_project/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2016 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': ' Timesheet Activities - Begin/End Hours on projects',
+    'summary': 'Adds starting and ending hours fields on the timesheet activities on projects',
+    'version': '8.0.0.1.0',
+    'category': 'Human Resources',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'hr_timesheet_activity_begin_end',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
