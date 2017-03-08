8d3307fa5824b4e7bfb33a921f97fc8cbfff5ba5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-03-08 11:43:02 +0200

    Improve domain filters

--- hr_timesheet_task_open_only/__openerp__.py
+++ hr_timesheet_task_open_only/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Timesheet only show open tasks',
     'summary': 'Only show open tasks on timesheets',
-    'version': '8.0.1.1.1',
+    'version': '8.0.1.1.2',
     'category': 'HR',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
