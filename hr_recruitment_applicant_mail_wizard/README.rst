b2c4c513e594a7c714480d4a7c220b5d4b10c334
Timo Talvitie <timo.talvitie@vizucom.com>
2017-11-02 15:09:47 +0200

    New module hr_recruitment_applicant_mail_wizard

---
+++ hr_recruitment_applicant_mail_wizard/README.rst
@@ -0,0 +1,42 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================================
+E-mail Wizard for Job Applications
+==================================
+
+* Adds a 'Send E-mail' button to Job Applications, utilizing the mail.compose.message wizard
+
+Installation
+============
+* Just install this module. 
+* Optionally, you can also install https://github.com/OCA/social/tree/10.0/mail_optional_autofollow and https://github.com/OCA/social/tree/10.0/mail_optional_follower_notification
+
+Configuration
+=============
+* By itself the module is very basic, but you can override the hr.applicant functions to e.g. enable limiting button visibility to specific stages or to use different e-mail templates for different stages.
+
+Usage
+=====
+\- 
+
+Known issues / Roadmap
+======================
+* None
+
+Credits
+=======
+
+Contributors
+------------
+* Timo Talvitie <timo.talvitie@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ hr_recruitment_applicant_mail_wizard/models/hr_applicant.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _, exceptions
+import uuid
+
+
+class HrApplicant(models.Model):
+
+    _inherit = 'hr.applicant'
+
+    @api.multi
+    def _get_show_email_button(self):
+        ''' Inherit this function to define more intricately when the button should be available '''
+        for applicant in self:
+            applicant.show_email_button = True
+
+    def _get_email_template(self):
+        ''' Inherit to use a custom e-mail template '''
+        return False
+
+    def _get_email_context(self):
+        ''' Inherit to use a custom context '''
+        template_id = self._get_email_template()
+        ctx = dict()
+        ctx.update({
+            'default_model': 'hr.applicant',
+            'default_res_id': self.id,
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+        })
+
+    @api.multi
+    def launch_wizard(self):
+        ''' Open the wizard for composing new e-mail '''
+        self.ensure_one()
+
+        try:
+            compose_form_id = self.env['ir.model.data'].get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+
+        ctx = self._get_email_context()
+
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+    show_email_button = fields.Boolean(compute=_get_show_email_button, string='Show the E-mail Applicant button')
