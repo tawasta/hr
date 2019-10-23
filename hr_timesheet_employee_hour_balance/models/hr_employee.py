a60590b87dfcc175cd664d9f0bd40651469c3e0d
difbit <36134879+difbit@users.noreply.github.com>
2019-10-23 13:56:06 +0300

    [IMP] Only approved timesheets are counted (#6)

--- hr_timesheet_employee_hour_balance/models/hr_employee.py
+++ hr_timesheet_employee_hour_balance/models/hr_employee.py
@@ -8,42 +8,47 @@ class Employee(models.Model):
     _inherit = 'hr.employee'
 
     def get_hours_worked(self, today):
-        ''' Fetch all timesheet lines the user has logged between calculation start date and today,
-        and get the sum of hours.
+        ''' Find all timesheets that have been approved. Iterate through their
+        timesheet lines and add up the logged hours '''
+        timesheet_model = self.env['hr_timesheet_sheet.sheet']
+        timesheet_ids = timesheet_model.search(args=[('user_id', '=', self.user_id.id),
+                                                     ('date_to', '>=', self.hour_balance_start),
+                                                     ('state', '=', 'done')], order='date_from ASC')
 
-        Override this function if there is need for more complex calculation
-        that takes into account e.g. public holidays. '''
+        hours_worked = 0
 
-        today_str = datetime.datetime.strftime(today, '%Y-%m-%d')
-
-        timesheet_line_ids = self.env['hr.analytic.timesheet'].search(args=[('user_id', '=', self.user_id.id),
-                                                                            ('date', '>=', self.hour_balance_start),
-                                                                            ('date', '<=', today_str)])
-
-        hours_worked = sum(timesheet_line.unit_amount for timesheet_line in timesheet_line_ids)
+        for timesheet in timesheet_ids:
+            hours_worked += sum(timesheet_line.unit_amount for timesheet_line in timesheet.timesheet_ids)
 
         return hours_worked
 
     def get_hours_needed(self, date_to_check, today, daily_hours):
-        ''' Iterate through all dates from start date to today. If the date is not
-        Saturday or Sunday, increase the hours_needed counter by the employee's
-        weekly working hours divided by number of weekly working days(=5).
-
-        Override this function if there is need for more complex calculation
-        that takes into account e.g. public holidays. '''
+        ''' Iterate through all timesheets that have been approved. Add up
+        all the weekdays in them and multiply them by the employee's daily
+        working time to get the target working hours. '''
 
         hours_needed = 0
 
-        while True:
-            if date_to_check.weekday() not in [5, 6]:
-                hours_needed += daily_hours
-            date_to_check += datetime.timedelta(days=1)
-            if date_to_check > today:
-                break
+        timesheet_model = self.env['hr_timesheet_sheet.sheet']
+        timesheet_ids = timesheet_model.search(args=[('user_id', '=', self.user_id.id),
+                                                     ('date_to', '>=', date_to_check),
+                                                     ('state', '=', 'done')], order='date_from ASC')
+
+        for timesheet in timesheet_ids:
+            date_to_check = datetime.datetime.strptime(timesheet.date_from, "%Y-%m-%d").date()
+            date_end = datetime.datetime.strptime(timesheet.date_to, "%Y-%m-%d").date()
+
+            while True:
+                ''' Skip saturday and sunday '''
+                if date_to_check.weekday() not in [5, 6]:
+                    hours_needed += daily_hours
+                date_to_check += datetime.timedelta(days=1)
+                if date_to_check > date_end:
+                    break
 
         return hours_needed
 
-    @api.depends('weekly_working_time', 'hour_balance_start', 'timesheet_ids', 'timesheet_ids.timesheet_ids')
+    @api.depends('weekly_working_time', 'hour_balance_start', 'timesheet_ids', 'timesheet_ids.timesheet_ids', 'timesheet_ids.state')
     @api.one
     def _get_hour_balance(self):
         ''' Calculates how many hours the employee has logged from hour_balance_start date
@@ -70,6 +75,15 @@ class Employee(models.Model):
     def _get_show_balance(self):
         self.show_balance = self.user_id.id == self.env.uid and True or False
 
+    @api.depends('timesheet_ids', 'timesheet_ids.timesheet_ids', 'timesheet_ids.state')
+    @api.one
+    def _get_hour_balance_end(self):
+        ''' Fetch the latest approved timesheet's end date '''
+        timesheet_model = self.env['hr_timesheet_sheet.sheet']
+        latest_timesheet = timesheet_model.search(args=[('user_id', '=', self.user_id.id),
+                                                        ('state', '=', 'done')], order='date_to DESC', limit=1)
+        self.hour_balance_end = latest_timesheet and latest_timesheet[0].date_to or False
+
     weekly_working_time = fields.Selection([('30', '30'),
                                             ('37.5', '37,5'),
                                             ('hour_worker', 'No fixed working time')], 'Weekly working time (h)', default='hour_worker')
@@ -83,23 +97,4 @@ class Employee(models.Model):
     # A helper field that is used to restrict that the user can only see their own hour balance in the employee form view
     show_balance = fields.Boolean('Show Hour Balance', compute=_get_show_balance)
 
-
-    @api.depends('timesheet_ids', 'timesheet_ids.timesheet_ids', 'timesheet_ids.state')
-    @api.one
-    def _get_hour_balance_end(self):
-        ''' Fetch the latest approved timesheet's end date '''
-        timesheet_model = self.env['hr_timesheet_sheet.sheet']
-        latest_timesheet = timesheet_model.search(args=[('user_id', '=', self.user_id.id),
-                                                        ('state', '=', 'done')], order='date_to DESC', limit=1)
-        self.hour_balance_end = latest_timesheet and latest_timesheet[0].date_to or False
-
-    @api.depends('weekly_working_time', 'hour_balance_start', 'timesheet_ids', 'timesheet_ids.timesheet_ids', 'time    sheet_ids.state')
-    @api.one
-    def _get_hour_balance(self):
-        ''' Simple override to add timesheet_ids.state to api.depends list '''
-        return super(Employee, self)._get_hour_balance()
-
-    hour_balance_end = fields.Date('Latest approved timesheet', compute=_get_hour_balance_end, store=True, help="Th    is is the ending date of the user's latest approved timesheet.")
-
-
-
+    hour_balance_end = fields.Date('Latest approved timesheet', compute=_get_hour_balance_end, store=True, help="This is the ending date of the user's latest approved timesheet.")
