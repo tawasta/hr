307ee6f08d59338e7e4a252280e1e258836e6e34
difbit <36134879+difbit@users.noreply.github.com>
2019-10-29 11:48:49 +0200

    12.0 dev hour balance (#7)
    * [ADD] init-file
    
    * [ADD] The module descriptor file
    
    * [ADD] init-file
    
    * [ADD] Python file
    
    * [ADD] View file
    
    * [REM] Files deleted
    
    * [ADD] init-file
    
    * [ADD] The module descriptor file
    
    * [ADD] init-file
    
    * [ADD] Python file
    
    * [ADD] View file
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Indentations improved
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Formatting
    
    * [IMP] Description
    
    * [IMP] Formatting
    
    * [IMP] Docstrings
    
    * [IMP] Strings

---
+++ hr_timesheet_employee_hour_balance/__manifest__.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2018 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'Employee Hour Balance',
+    'summary': """
+Employee Hour Balance
+=====================
+ * Shows the hour balance for each employee from a certain date onwards, based
+ on their weekly working hours
+ * Adds two new groups, one for seeing own balance, and another for seeing all
+ employees' balances
+    """,
+    'version': '12.0.1.0.0',
+    'category': 'HR',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'hr_timesheet_sheet',
+        'resource',
+    ],
+    'data': [
+        'views/hr_employee.xml',
+    ],
+}
