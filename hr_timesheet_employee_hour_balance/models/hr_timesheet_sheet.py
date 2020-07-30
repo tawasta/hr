4eaedfb8e076e823d553be3608f86c0efd750455
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-07-30 10:49:27 +0300

    [REM] Clean up obsolete parts of hr_timesheet_employee_hour_balance

--- hr_timesheet_employee_hour_balance/models/hr_timesheet_sheet.py
+++ hr_timesheet_employee_hour_balance/models/hr_timesheet_sheet.py
@@ -5,36 +5,30 @@ from odoo import models
 
 class HrTimesheetSheet(models.Model):
 
-    _inherit = 'hr_timesheet.sheet'
+    _inherit = "hr_timesheet.sheet"
 
     calendar_id = fields.Many2one(
-        comodel_name='resource.calendar',
-        compute='_compute_calendar_id',
+        comodel_name="resource.calendar",
+        compute="_compute_calendar_id",
         store=True,
         readonly=False,
     )
 
-    total_hours = fields.Float(
-        related='calendar_id.total_hours',
-        readonly=True,
-    )
+    total_hours = fields.Float(related="calendar_id.total_hours", readonly=True,)
 
     total_remaining = fields.Float(
-        string='Remaining',
-        compute='_compute_total_remaining',
-        store=True
+        string="Remaining", compute="_compute_total_remaining", store=True
     )
 
-    @api.onchange('employee_id')
-    @api.depends('employee_id')
+    @api.onchange("employee_id")
+    @api.depends("employee_id")
     def _compute_calendar_id(self):
         for record in self:
             if record.employee_id:
                 record.calendar_id = record.employee_id.resource_calendar_id
 
-    @api.depends('timesheet_ids.unit_amount', 'calendar_id')
+    @api.depends("timesheet_ids.unit_amount", "calendar_id")
     def _compute_total_remaining(self):
         for record in self:
             if record.total_time and record.total_hours:
-                record.total_remaining = \
-                    record.total_time - record.total_hours
+                record.total_remaining = record.total_time - record.total_hours
