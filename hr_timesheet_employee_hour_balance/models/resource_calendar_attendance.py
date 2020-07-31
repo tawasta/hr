4fa181660bb2e8bd6e23885aae8a8e8780533c4b
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-07-31 14:00:29 +0300

    [ADD] Restore missing parts, that were removed accidentally

---
+++ hr_timesheet_employee_hour_balance/models/resource_calendar_attendance.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+
+
+class ResourceCalendarAttendance(models.Model):
+
+    _inherit = "resource.calendar.attendance"
+
+    total_hours = fields.Float(string="Total hours", help="Total hours within the day")
+
+    @api.onchange("hour_from", "hour_to")
+    def _compute_total_hours(self):
+        for record in self:
+            if record.hour_from and record.hour_to:
+                record.total_hours = record.hour_to - record.hour_from
