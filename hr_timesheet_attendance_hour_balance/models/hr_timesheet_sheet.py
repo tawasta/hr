035b3fb302a32577c0a9cce2b044b17e3c8a3e97
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-10-30 12:23:42 +0200

    [ADD] Cumulative attendance balance

--- hr_timesheet_attendance_hour_balance/models/hr_timesheet_sheet.py
+++ hr_timesheet_attendance_hour_balance/models/hr_timesheet_sheet.py
@@ -11,6 +11,11 @@ class HrTimesheetSheet(models.Model):
         string="Attendance balance", compute="_compute_total_attendance_balance",
     )
 
+    cumulative_attendance_balance = fields.Float(
+        string="Cumulative Attendance",
+        compute="_compute_cumulative_attendance_balance",
+    )
+
     @api.depends("timesheet_ids.unit_amount", "calendar_id")
     def _compute_total_attendance_balance(self):
         for record in self:
@@ -18,3 +23,20 @@ class HrTimesheetSheet(models.Model):
                 record.total_attendance_balance = (
                     record.total_attendance - record.total_hours
                 )
+
+    def _compute_cumulative_attendance_balance(self):
+        for record in self:
+            domain = [
+                ("employee_id", "=", record.employee_id.id),
+                ("date_end", "<=", record.date_end),
+                # Count only done timesheets?
+                # "|",
+                # ("state", "=", "done"),
+                # Show current draft timesheet balance
+                # ("id", "=", record.id),
+            ]
+
+            timesheets = self.search(domain)
+            record.cumulative_attendance_balance = sum(
+                timesheets.mapped("total_attendance_balance")
+            )
