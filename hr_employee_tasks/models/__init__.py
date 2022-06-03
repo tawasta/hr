05aba7e7bde8807e1a6c7d415bc7fee57f66eeeb
difbit <timo.kekalainen@hotmail.com>
2022-06-03 09:53:12 +0300

    [MIG] Created 14.0 branch

--- hr_employee_tasks/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import hr_employee
-from . import project_task
-from . import user
--- hr_timesheet_employee_hour_balance/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import hr_employee
-from . import hr_timesheet_sheet
-from . import resource_calendar_attendance
