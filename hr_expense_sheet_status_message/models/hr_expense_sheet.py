9dba00b5bcd0163d3a8e39b2a5459d7643b104a5
difbit <timo.kekalainen@tawasta.fi>
2025-09-11 15:40:53 +0300

    [FIX] hr_expense_sheet_status_message module: Fixed mail template ref

--- hr_expense_sheet_status_message/models/hr_expense_sheet.py
+++ hr_expense_sheet_status_message/models/hr_expense_sheet.py
@@ -27,7 +27,7 @@ class HrExpenseSheet(models.Model):
 
         for sheet in self:
             sheet.message_post_with_source(
-                "hr_expense_sheet_status_message.hr_expense_template_approve_message",
+                "hr_expense_sheet_status_message.hr_expense_template_message_approve",
                 subtype_id=subtype_id,
                 render_values={"name": sheet.name},
             )
