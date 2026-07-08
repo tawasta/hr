7eb404e302f9b11e55db36267e99cd8ca45c10d4
Joonas Lahtinen <joonas.lahtinen@futural.fi>
2026-07-08 23:41:48 +0300

    [FIX] committed wrong version of the module initially

--- hr_autofill_employee_job_title/README.rst
+++ hr_autofill_employee_job_title/README.rst
@@ -1,7 +1,12 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
 ===========================
 Hr Autofill Employee Job Title
 ===========================
-Automatically fill a employee job title, when creation is done via portal.
+Automatically fill a employee job title, when creation is done via portal 
+expense reporting.
 
 Installation
 ============
@@ -9,13 +14,25 @@ Installation
 
 Configuration
 =============
-* In the Settings -> Expenses -> Accounting, configure the automatic job title
+* In the Settings -> Employees, configure the automatic job title
 
 Usage
 =====
 * Install module
 * Configure the job title in the settings
+   * Add new titles in Employee -> Settings -> Job Titles, if needed
 * Add employees from the portal
 * Added employees get the title set in the settings
 
+Contributors
+------------
+* Joonas Lahtinen <joonas.lahtinen@futural.fi>
 
+Maintainer
+----------
+
+.. image:: https://futural.fi/templates/tawastrap/images/logo.png
+   :alt: Futural Oy
+   :target: https://futural.fi/
+
+This module is maintained by Futural Oy.
--- hr_autofill_employee_job_title/models/hr_expense.py
+++ hr_autofill_employee_job_title/models/hr_expense.py
@@ -9,24 +9,40 @@ class HrExpense(models.Model):
         expenses = super().create(vals_list)
 
         if self.env.context.get("website_id"):
-            default_title = (
+            default_job_id_str = (
                 self.env["ir.config_parameter"]
                 .sudo()
-                .get_param("hr_autofill_employee_job_title.default_job_title", "")
+                .get_param("hr_autofill_employee_job_title.default_job_id", "")
             )
-            if default_title:
-                employees_to_update = self.env["hr.employee"]
-                for expense in expenses:
-                    if expense.employee_id and not expense.employee_id.job_title:
-                        employees_to_update |= expense.employee_id
-                if employees_to_update:
-                    employees_to_update.write({"job_title": default_title})
-                    employees_to_update.message_post(
-                        body=_(
-                            "This user was automatically given a job title "
-                            "%s as a result of submitting an expense report.",
-                            default_title,
-                        )
-                    )
 
+            if default_job_id_str:
+                try:
+                    default_job = (
+                        self.env["hr.job"]
+                        .sudo()
+                        .browse(int(default_job_id_str))
+                        .exists()
+                    )
+                except (ValueError, TypeError):
+                    default_job = self.env["hr.job"]
+
+                if default_job:
+                    employees_to_update = self.env["hr.employee"]
+                    for expense in expenses:
+                        if expense.employee_id and not expense.employee_id.job_id:
+                            employees_to_update |= expense.employee_id
+                    if employees_to_update:
+                        employees_to_update.write(
+                            {
+                                "job_id": default_job.id,
+                                "job_title": default_job.name,
+                            }
+                        )
+                        employees_to_update.message_post(
+                            body=_(
+                                "This user was automatically assigned to job position "
+                                "%s as a result of submitting an expense report.",
+                                default_job.name,
+                            )
+                        )
         return expenses
